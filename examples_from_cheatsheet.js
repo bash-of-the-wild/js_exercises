309e5efe022b6c7af429a567d984a6e665f4427e
JackMarx <seriousfools@gmail.com>
2019-05-29 12:01:49 -0500

    initial commit

---
+++ examples_from_cheatsheet.js
@@ -0,0 +1,22 @@
+// var i = 0;
+
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+for(var i = 0; i < 50; i++) {
+  console.log(i);
+}
+
+
+// var x;
+
+// function doSomething() {
+//   x = "Zebra";
+// }
+
+// doSomething();
+// console.log(x);    // x is "Zebra"
+
